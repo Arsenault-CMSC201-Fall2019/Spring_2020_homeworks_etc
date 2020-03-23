aac0284cea1fd93a3256370da43afbd6185f6a6b
Arsenault-CMSC201-Fall2019 <55154033+Arsenault-CMSC201-Fall2019@users.noreply.github.com>
2020-03-23 17:47:41 -0400

    Add files via upload

---
+++ lab6.py
@@ -0,0 +1,65 @@
+
+"""
+
+File:           FILENAME.py
+Author:         YOUR NAME
+Date:           TODAYS DATE
+Section:        YOUR SECTION NUMBER
+E-mail:         YOUR UMBC E-MAIL ADDRESS
+Description:    A DESCRIPTION OF WHAT THIS PROGRAM DOES
+"""
+
+def  sum_a_list(numbers):
+    """
+        Finds the sum of a list of integers
+        parameter: a list of integers
+        return: the sum of the list of integers
+    """
+
+def  get_string_lengths(strings):
+    """
+        Gets the lengths of each string in a list
+        parameter: a list of strings:
+        return:  a list of integers containing the length of each string
+    """
+
+def get_names():
+    """
+        Asks the user for a list of names
+        Parameter: none
+        Return: a list of strings containing the names the user entered
+    """
+
+if __name__ == "__main__":
+    physicists = [
+           'Einstein',
+            'Compton',
+            'Fermi',
+            'Bohr',
+            'Heisenberg'
+            'Schrodinger'
+    ]
+
+#write the function calls to determine how many letters there
+# are in the list of physicists
+
+# print out the number of letters, with appropriate descriptive text
+
+    chemists = [
+            'Curie',
+            'Roentgen',
+            'Pauling',
+            'Rutherford',
+            'Joliot-Curie'
+    ]
+
+# write the function calls to determine how many letters
+# there are in the list of chemists
+
+# print out the number of letters, with appropriate descriptive text
+
+# write a function call to prompt the user to enter a list of names
+# then call the functions to calculate how many letters are in the list
+# entered by the user, and print that number with appropriate
+# descriptive text
+
